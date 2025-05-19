5a970aa8fd89f497969ee27118297661160f83d5
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-05-19 18:12:36 -0300

    cria componente ticketitem dos chamados

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import LayoutPrivate from "@/components/admin/layoutPrivate";
+import { TicketItem } from "@/components/admin/tikectItem";
 import { Container } from "@/components/container";
 import { canSSRAuth } from "data/utils/canRAuth";
 import Head from "next/head";
+import Link from "next/link";
 import React, { useState, useEffect, useContext } from "react";
 import useWebSocket from "react-use-websocket";
 
@@ -32,6 +34,35 @@ export default function Dashboard() {
       </Head>
       <LayoutPrivate>
         <Container>
+          <main className="mt-9 mb-2">
+            <div className="flex items-center justify-between">
+              <h1 className="text-3xl font-bold">Chamados</h1>
+              <Link
+                href="/dashboard/new"
+                className=" bg-blue-500 px-4 py-1 rounded text-white"
+              >
+                Abrir chamado
+              </Link>
+            </div>
+
+            <table className="min-w-full my-2">
+              <thead>
+                <tr>
+                  <th className="font-medium text-left pl-1">CLIENTE</th>
+                  <th className="font-medium text-left hidden sm:block">
+                    CADASTRO
+                  </th>
+                  <th className="font-medium text-left">STATUS</th>
+                  <th className="font-medium text-left">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <TicketItem />
+                <TicketItem />
+              </tbody>
+            </table>
+          </main>
+
           <h2 className="border-t-2 border-2 rounded-xl border-green-500 mt-2 px-4 py-1">
             Eventos em Tempo Real
           </h2>
