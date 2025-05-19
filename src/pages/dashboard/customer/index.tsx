60dd48ce58d8ceb605d10351ee6daeaae09529a4
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-05-19 18:25:49 -0300

    cria pages estaticas customer new e componente cadcustomer

--- src/pages/dashboard/customer/index.tsx
+++ src/pages/dashboard/customer/index.tsx
@@ -1,12 +1,25 @@
+import { CardCustomer } from "@/components/admin/cardcustomer";
 import { Container } from "@/components/container";
+import Link from "next/link";
 
 export default function Customer() {
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Meus Clientes</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Meus Clientes</h1>
+          <Link
+            href="/dashboard/customer/new"
+            className="bg-blue-500 text-white px-3 py-1 rounded"
+          >
+            Novo cliente
+          </Link>
         </div>
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
+          <CardCustomer />
+          <CardCustomer />
+          <CardCustomer />
+        </section>
       </main>
     </Container>
   );
